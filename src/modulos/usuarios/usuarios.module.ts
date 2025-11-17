ab09e935930d6b54dda547dbb673026ad170ebd9
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-17 16:28:07 -0400

    feat: importar - actualizar importaciones de archivos del modulo usuario

--- src/modulos/usuarios/usuarios.module.ts
+++ src/modulos/usuarios/usuarios.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { UsuariosService } from './usuarios.service';
-import { UsuariosController } from './usuarios.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuariosService } from './usuarios-services/usuarios.service';
+import { UsuariosController } from './usuarios-controllers/usuarios.controller';
+import { Usuario } from './usuario-entities/usuario.entity';
+import { Persona } from './usuario-entities/persona.entity';
+import { Rol } from './usuario-entities/rol.entity';
+import { UsuarioRol } from './usuario-entities/usuario-rol.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Usuario, Persona, Rol, UsuarioRol])],
   controllers: [UsuariosController],
   providers: [UsuariosService],
+  exports: [UsuariosService],
 })
 export class UsuariosModule {}
