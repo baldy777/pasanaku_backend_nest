60dca1024c0be8ac98bfc1e4ddcbea312af1385e
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-17 16:24:30 -0400

    remove: quitar archivo de conexion a base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from '../ormconfig';
+import { UsuariosModule } from './modulos/usuarios/usuarios.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       ...AppDataSource.options,
+      autoLoadEntities: true,
     }),
+    UsuariosModule,
     
   ],
 })
