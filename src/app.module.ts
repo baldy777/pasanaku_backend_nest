e40f7479007f30575696e8e37d61a15345d861cf
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-17 16:25:29 -0400

    refactor: adicion de conexion nueva a base de datos con TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppDataSource } from '../ormconfig';
 import { UsuariosModule } from './modulos/usuarios/usuarios.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      ...AppDataSource.options,
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
     }),
     UsuariosModule,
-    
   ],
 })
 export class AppModule {}
