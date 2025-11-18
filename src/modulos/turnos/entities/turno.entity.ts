44616aeccda42bc64e0722ae8f0ac5258047caa8
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-18 02:15:21 -0400

    feat: adicion de entidad de turnos

--- src/modulos/turnos/entities/turno.entity.ts
+++ src/modulos/turnos/entities/turno.entity.ts
@@ -1,1 +1,25 @@
-export class Turno {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Aporte } from '../../aportes/entities/aporte.entity';
+import { BaseEntityAuditable } from 'src/modulos/BaseEntityAuditable';
+
+@Entity('turno')
+export class Turno extends BaseEntityAuditable {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  numeroTurno: number;
+
+  @Column({ type: 'date' })
+  fechaAsignada: Date;
+
+  @OneToOne(() => Aporte, (aporte) => aporte.turno)
+  @JoinColumn({ name: 'aporte_id' })
+  aporte: Aporte;
+}
