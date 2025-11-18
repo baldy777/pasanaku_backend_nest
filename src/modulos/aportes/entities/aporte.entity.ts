9c9948161a3cd65f7123eba3c99dcc1d53259869
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-18 02:06:02 -0400

    feat: adicion de entidad para modulo APORTES

--- src/modulos/aportes/entities/aporte.entity.ts
+++ src/modulos/aportes/entities/aporte.entity.ts
@@ -1,1 +1,26 @@
-export class Aporte {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Usuario } from '../../usuarios/usuario-entities/usuario.entity';
+import { BaseEntityAuditable } from 'src/modulos/BaseEntityAuditable';
+
+@Entity('aporte')
+export class Aporte extends BaseEntityAuditable {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  monto: number;
+
+  @Column({ type: 'date' })
+  fecha: Date;
+
+  @ManyToOne(() => Usuario, (usuario) => usuario.aportes)
+  @JoinColumn({ name: 'usuario_id' })
+  usuario: Usuario;
+}
