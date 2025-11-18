83902d112066d0a7e9a2c78b8a992d3ee8f4e3e0
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-18 02:07:02 -0400

    remove: quitar comentario innecesario

--- src/modulos/grupos/grupo-entities/grupo.entity.ts
+++ src/modulos/grupos/grupo-entities/grupo.entity.ts
@@ -1,1 +1,18 @@
-export class Grupo {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Usuario } from '../../usuarios/usuario-entities/usuario.entity';
+import { BaseEntityAuditable } from 'src/modulos/BaseEntityAuditable';
+
+@Entity('grupo')
+export class Grupo extends BaseEntityAuditable {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nombre: string;
+
+  @Column({ nullable: true })
+  descripcion: string;
+
+  @ManyToMany(() => Usuario, (usuario) => usuario.grupos)
+  usuarios: Usuario[];
+}
