8a9466b4490b0f989c25826059bb61ce47b06714
baldy777 <jhoelquispetapia315@gmail.com>
2025-12-04 17:27:20 -0400

    remove: eliminar modulo de historial

--- src/modulos/historial/dto/update-historial.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHistorialDto } from './create-historial.dto';
-
-export class UpdateHistorialDto extends PartialType(CreateHistorialDto) {}
--- src/modulos/historial/historial.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HistorialService } from './historial.service';
-import { HistorialController } from './historial.controller';
-
-@Module({
-  controllers: [HistorialController],
-  providers: [HistorialService],
-})
-export class HistorialModule {}
