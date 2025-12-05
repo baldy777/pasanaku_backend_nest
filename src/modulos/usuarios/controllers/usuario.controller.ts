bfeae0b1e621a7835fb8d833466beef5e25070a0
baldy777 <jhoelquispetapia315@gmail.com>
2025-12-05 18:34:10 -0400

    feat: adicion de protecion de endpoints con JWT en controlador usuarios

--- src/modulos/usuarios/controllers/usuario.controller.ts
+++ src/modulos/usuarios/controllers/usuario.controller.ts
@@ -8,34 +8,24 @@ import {
   Param,
   NotFoundException,
   BadRequestException,
+  UseGuards,
 } from '@nestjs/common';
 import { UsuarioService } from '../services/usuario.service';
 import { CreateUsuarioDto } from '../dto/usuarios/crearUsuario.dto';
 import { ActualizarUsuarioDto } from '../dto/usuarios/actualizarUsuario.dto';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { RolesGuard } from '../guards/roles.guard';
+import { Roles } from '../decorators/roles.decorator';
 
 @Controller('usuarios')
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
-  @Get('verify/:token')
-  async verifyEmail(@Param('token') token: string) {
-    const usuario = await this.usuarioService.findByToken(token);
-
-    if (!usuario) {
-      throw new NotFoundException('Token inválido');
-    }
-
-    if (
-      usuario.verificacionExpirada &&
-      usuario.verificacionExpirada < new Date()
-    ) {
-      throw new BadRequestException('Token expirado');
-    }
-
-    await this.usuarioService.verifyUsuario(usuario);
-
-    return { message: 'Correo verificado correctamente' };
+  @Post()
+  create(@Body() crearUsuarioDto: CreateUsuarioDto) {
+    return this.usuarioService.create(crearUsuarioDto);
   }
+
   @Post('verificar-codigo')
   async verificarCodigo(@Body() body: { correo: string; codigo: string }) {
     const usuario = await this.usuarioService.findByCorreo(body.correo);
@@ -58,22 +48,42 @@ export class UsuarioController {
     return { message: 'Cuenta verificada correctamente' };
   }
 
+  @Get('verify/:token')
+  async verifyEmail(@Param('token') token: string) {
+    const usuario = await this.usuarioService.findByToken(token);
+
+    if (!usuario) {
+      throw new NotFoundException('Token inválido');
+    }
+
+    if (
+      usuario.verificacionExpirada &&
+      usuario.verificacionExpirada < new Date()
+    ) {
+      throw new BadRequestException('Token expirado');
+    }
+
+    await this.usuarioService.verifyUsuario(usuario);
+
+    return { message: 'Correo verificado correctamente' };
+  }
+
   @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('Administrador')
   findAll() {
     return this.usuarioService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: number) {
     return this.usuarioService.findOne(id);
   }
 
-  @Post()
-  create(@Body() crearUsuarioDto: CreateUsuarioDto) {
-    return this.usuarioService.create(crearUsuarioDto);
-  }
-
   @Put(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('Administrador')
   update(
     @Param('id') id: number,
     @Body() actualizarUsuarioAllDto: ActualizarUsuarioDto,
@@ -82,6 +92,8 @@ export class UsuarioController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('Administrador')
   partialUpdate(
     @Param('id') id: number,
     @Body() actualizarUsuarioOneDto: ActualizarUsuarioDto,
@@ -90,11 +102,15 @@ export class UsuarioController {
   }
 
   @Post(':id/roles/:rolId')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('Administrador')
   asignarRol(@Param('id') usuarioId: number, @Param('rolId') rolId: number) {
     return this.usuarioService.asignarRol(usuarioId, rolId);
   }
 
   @Patch(':id/roles/:rolId/quitar')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('Administrador')
   quitarRol(@Param('id') usuarioId: number, @Param('rolId') rolId: number) {
     return this.usuarioService.quitarRol(usuarioId, rolId);
   }
